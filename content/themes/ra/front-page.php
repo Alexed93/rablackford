b6d4f0e6cf606b766772bee3e8c520430233d9b7
Alex Edwards <alexed93@gmail.com>
2017-04-23 16:58:38 +0100

    Added introductory section homepage

--- content/themes/ra/front-page.php
+++ content/themes/ra/front-page.php
@@ -17,26 +17,17 @@
 // Get the header
 get_header();
 
+$intro_title = get_field('intro_title');
+$intro_para = get_field('intro_para');
+
 ?>
 
 <main class="section">
     <div class="container">
-        <?php if ( have_posts() ): ?>
-            <?php while ( have_posts() ): ?>
-                <?php the_post(); ?>
-                <article>
-                    <?php the_title(); ?>
-
-                    <?php if ( $post->post_excerpt ): ?>
-                        <?php echo get_the_excerpt(); ?>
-                    <?php endif; ?>
-
-                    <?php the_content(); ?>
-                </article>
-            <?php endwhile; ?>
-        <?php else: ?>
-            <?php get_template_part('views/errors/404-posts'); ?>
-        <?php endif; ?>
+        <div class="container--introduction">
+            <h1 class="headline"><?php echo $intro_title; ?></h1>
+            <p class="gamma"><?php echo $intro_para; ?></p>
+        </div>
     </div>
     <!-- .container -->
 </main>
