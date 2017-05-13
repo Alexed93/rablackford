6ee0f6a8084b43f94f9b61b10d73d4e9a4ba2ce1
Alex Edwards <alexed93@gmail.com>
2017-05-13 22:06:20 +0100

    Added file upload functionality to featured card

--- content/themes/ra/views/cards/card-featured.php
+++ content/themes/ra/views/cards/card-featured.php
@@ -12,8 +12,18 @@
 
 $card_title = get_field('cardfeatured_title', 'options');
 $card_text = get_field('cardfeatured_text', 'options');
-$card_btn = get_field('cardfeatured_button', 'options');
-$card_btnlink = get_field('cardfeatured_link', 'options');
+$card_buttontext = get_field('cardfeatured_button', 'options');
+$card_button = get_field('cardfeatured_buttonquestion', 'options');
+$card_file = get_field('cardfeatured_filequestion', 'options');
+
+if ( $card_button ):
+    $card_btnlink = get_field('cardfeatured_link', 'options');
+endif;
+
+if ( $card_file ):
+    $card_file = get_field('cardfeatured_file', 'options');
+    $card_fileurl = $card_file['url'];
+endif;
 
 ?>
 
@@ -26,9 +36,11 @@ $card_btnlink = get_field('cardfeatured_link', 'options');
             <?php if( $card_text ): ?>
                 <p><?php echo $card_text; ?></p>
             <?php endif; ?>
-            <a href="<?php echo $card_btnlink; ?>" class="btn btn--primary">
-                <?php echo $card_btn; ?>
-            </a>
+            <?php if ( $card_button || $card_fileurl ): ?>
+                <a href="<?php echo $card_btnlink; ?><?php echo $card_fileurl; ?>" class="btn btn--primary">
+                    <?php echo $card_buttontext; ?>
+                </a>
+            <?php endif; ?>
         </div>
     </div>
 <?php endif; ?>
