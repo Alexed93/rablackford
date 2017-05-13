7be62b7ad624423be6abab125ab6ec3392c52be6
Alex Edwards <alexed93@gmail.com>
2017-05-13 22:25:50 +0100

    Added larger ACM badge on homepage at client request

--- content/themes/ra/front-page.php
+++ content/themes/ra/front-page.php
@@ -23,14 +23,17 @@ $intro_para = get_field('intro_para');
 ?>
 
 <main class="section">
-    <div class="container">
-        <div class="container--introduction">
+    <div class="container u-space-top cf">
+        <a href="http://solidfuel.co.uk/approved-coal-wood-merchants/" class="u-float-left">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/acm--large.png" title="Approved Coal Merchant" alt="Approved Coal Merchant" class="acm-home">
+        </a>
+        <div class="home-intro u-float-left">
             <h1 class="headline"><?php echo $intro_title; ?></h1>
             <p class="introduction_text"><?php echo $intro_para; ?></p>
         </div>
     </div>
 
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/frontpic.svg" title="RA Blackford and son" alt="RA Blackford and son" class="u-push-bottom@2 u-width-100">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/frontpic.svg" title="RA Blackford and son" alt="RA Blackford and son" class="u-push-bottom@2 u-width-100 u-push-top@2">
 
     <div class="container">
         <?php get_template_part( 'views/cards/card-introductory' ); ?>
